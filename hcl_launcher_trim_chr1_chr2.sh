df86dac8fcb3f0db2f93c1615c70e196132adbda
HamLam <lamx0031@umn.edu>
2017-11-30 12:53:49 -0600

    Update hcl_launcher_trim_chr1_chr2.sh

--- hcl_launcher_trim_chr1_chr2.sh
+++ hcl_launcher_trim_chr1_chr2.sh
@@ -165,6 +165,9 @@ echo $ordered_genes > "$sample_path/ordered_genes_temp.txt"
 # Replace comma with newline so we can load it into a MySQL database
 tr , '\n' < "$sample_path/ordered_genes_temp.txt" > "$sample_path/ordered_genes.txt"
 #cp $template_pwd/chr1_chr2_ordered_genes.txt $sample_path/ordered_genes.txt
+mv $sample_path/ordered_genes.txt $sample_path/ordered_genes.txt.orig
+perl $scripts_location/find_chr1_2.pl $template_pwd/TSOV2_Genes_With_Chr_listed $sample_path/ordered_genes.txt.orig
+mv $sample_path/chr1_2_genes_file.txt $sample_path/ordered_genes.txt
 
 # Delete the temp file
 rm -rf "$sample_path/ordered_genes_temp.txt"
